cbe64441363d1cc706f83820b0b4726b5a799cb6
khuram787 <khuramssultan@gmail.com>
2025-11-26 10:09:51 +0500

    Update settings.js

--- settings.js
+++ settings.js
@@ -1,5 +1,5 @@
 module.exports = {
-  SESSION_ID: "", // Put Session Id Here Inside "*"
+  SESSION_ID: "", // Put Session Id Here Inside "Ice~Ho66L3"
   OWNER_NUMBER: "263719647303", // Put Owner Number Here 
   PREFIX: ".", // Default Prefix You Can Change To .,*!?+-&%#@
   CDN: "https://mrfrankk-cdn.hf.space" // Dont change this part
